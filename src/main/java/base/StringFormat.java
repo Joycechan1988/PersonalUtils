567dbdbb913d10effb250dd79acc304dbff63c06
hchen11 <hchen11@paypal.com>
2016-04-20 16:41:40 +0800

    add string format method

---
+++ src/main/java/base/StringFormat.java
@@ -0,0 +1,40 @@
+package base;
+
+import com.sun.istack.internal.Nullable;
+
+/**
+ * format("test %s %s", 1, 2)
+ */
+public class StringFormat {
+    public static String format(String template, @Nullable Object... args) {
+        template = String.valueOf(template); // null -> "null"
+
+        // start substituting the arguments into the '%s' placeholders
+        StringBuilder builder = new StringBuilder(template.length() + 16 * args.length);
+        int templateStart = 0;
+        int i = 0;
+        while (i < args.length) {
+            int placeholderStart = template.indexOf("%s", templateStart);
+            if (placeholderStart == -1) {
+                break;
+            }
+            builder.append(template, templateStart, placeholderStart);
+            builder.append(args[i++]);
+            templateStart = placeholderStart + 2;
+        }
+        builder.append(template, templateStart, template.length());
+
+        // if we run out of placeholders, append the extra args in square braces
+        if (i < args.length) {
+            builder.append(" [");
+            builder.append(args[i++]);
+            while (i < args.length) {
+                builder.append(", ");
+                builder.append(args[i++]);
+            }
+            builder.append(']');
+        }
+
+        return builder.toString();
+    }
+}
